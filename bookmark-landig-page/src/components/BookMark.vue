b7739de2af26b8d4fd797363adffb344646595ee
Tolobinha <xtrubulho@protonmail.com>
2024-07-28 22:34:00 -0300

    first commit

---
+++ bookmark-landig-page/src/components/BookMark.vue
@@ -0,0 +1,328 @@
+<template>
+  <div class="container">
+    <header class="container-header">
+      <span class="container-header__title">
+        <svg width="148" height="25" xmlns="http://www.w3.org/2000/svg">
+          <g fill="none" fill-rule="evenodd">
+            <path
+              d="M37 6.299h5.227c.746 0 1.434.155 2.062.466.629.311 1.123.735 1.484 1.27s.542 1.12.542 1.754c0 .672-.165 1.254-.495 1.746-.33.491-.762.868-1.297 1.129v.15c.697.248 1.25.643 1.661 1.185.41.541.616 1.191.616 1.95 0 .735-.196 1.385-.588 1.951a3.817 3.817 0 0 1-1.587 1.307c-.665.305-1.403.457-2.212.457H37V6.299zm5.04 5.45c.548 0 .986-.152 1.316-.457.33-.305.495-.688.495-1.148 0-.448-.159-.824-.476-1.13-.318-.304-.738-.457-1.26-.457H39.52v3.192h2.52zm.28 5.619c.61 0 1.086-.159 1.428-.476.342-.317.513-.731.513-1.241 0-.51-.174-.927-.522-1.251-.349-.324-.847-.485-1.494-.485H39.52v3.453h2.8zm12.927 2.595c-1.307 0-2.492-.308-3.556-.924a6.711 6.711 0 0 1-2.511-2.53c-.61-1.07-.915-2.246-.915-3.528 0-1.281.305-2.457.915-3.528a6.711 6.711 0 0 1 2.51-2.529C52.756 6.308 53.94 6 55.248 6c1.306 0 2.492.308 3.556.924a6.711 6.711 0 0 1 2.51 2.53c.61 1.07.915 2.246.915 3.527 0 1.282-.305 2.458-.915 3.528a6.711 6.711 0 0 1-2.51 2.53c-1.064.616-2.25.924-3.556.924zm0-2.39a4.52 4.52 0 0 0 2.258-.578 4.177 4.177 0 0 0 1.615-1.624c.392-.697.588-1.494.588-2.39 0-.896-.196-1.692-.588-2.389a4.177 4.177 0 0 0-1.615-1.624 4.52 4.52 0 0 0-2.258-.579 4.47 4.47 0 0 0-2.25.579 4.195 4.195 0 0 0-1.605 1.624c-.392.697-.588 1.493-.588 2.39 0 .895.196 1.692.588 2.389a4.195 4.195 0 0 0 1.605 1.624 4.47 4.47 0 0 0 2.25.578zm15.353 2.39c-1.307 0-2.492-.308-3.556-.924a6.711 6.711 0 0 1-2.51-2.53c-.61-1.07-.915-2.246-.915-3.528 0-1.281.305-2.457.914-3.528a6.711 6.711 0 0 1 2.511-2.529C68.108 6.308 69.294 6 70.6 6c1.307 0 2.492.308 3.556.924a6.711 6.711 0 0 1 2.51 2.53c.61 1.07.915 2.246.915 3.527 0 1.282-.305 2.458-.914 3.528a6.711 6.711 0 0 1-2.511 2.53c-1.064.616-2.25.924-3.556.924zm0-2.39a4.52 4.52 0 0 0 2.259-.578 4.177 4.177 0 0 0 1.614-1.624c.392-.697.588-1.494.588-2.39 0-.896-.196-1.692-.588-2.389a4.177 4.177 0 0 0-1.614-1.624 4.52 4.52 0 0 0-2.259-.579 4.47 4.47 0 0 0-2.25.579 4.195 4.195 0 0 0-1.605 1.624c-.392.697-.588 1.493-.588 2.39 0 .895.196 1.692.588 2.389a4.195 4.195 0 0 0 1.606 1.624 4.47 4.47 0 0 0 2.249.578zM79.83 6.3h2.52v5.73h.15l4.89-5.73h3.043v.149L85.6 11.973l5.338 7.542v.149h-3.08l-3.994-5.693-1.512 1.773v3.92h-2.52V6.299zM93.779 6h3.248l3.546 9.39h.15L104.268 6h3.267v13.365h-2.501v-6.589l.15-2.221h-.15l-3.398 8.81h-1.96l-3.416-8.81h-.149l.15 2.221v6.59h-2.483V6zm20.8 0h2.894l5.021 13.365h-2.781l-1.12-3.192h-5.115l-1.12 3.192h-2.781L114.579 6zm3.193 7.859l-1.176-3.36-.486-1.606h-.149l-.485 1.606-1.195 3.36h3.49zM124.553 6h4.872c.871 0 1.646.18 2.324.541.678.361 1.204.862 1.577 1.503.374.64.56 1.366.56 2.175 0 .858-.27 1.62-.812 2.286a4.617 4.617 0 0 1-2.044 1.447l-.018.13 3.584 5.134v.15h-2.894l-3.453-5.022h-1.176v5.021h-2.52V6zm4.853 6.03c.573 0 1.04-.175 1.4-.523.361-.349.542-.79.542-1.326 0-.51-.172-.945-.514-1.306-.342-.361-.806-.542-1.39-.542h-2.371v3.696h2.333zm7.23-6.03h2.52v5.73h.15l4.89-5.73h3.043v.15l-4.835 5.525 5.34 7.541v.15h-3.08l-3.996-5.694-1.512 1.773v3.92h-2.52V6z"
+              fill="#242A45"
+              fill-rule="nonzero"
+            />
+            <g>
+              <circle fill="#5267DF" cx="12.5" cy="12.5" r="12.5" />
+              <path
+                d="M9 9v10l3.54-3.44L16.078 19V9a2 2 0 0 0-2-2H11a2 2 0 0 0-2 2z"
+                fill="#FFF"
+              />
+            </g>
+          </g>
+        </svg>
+      </span>
+      <span class="container-menu">
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="15">
+          <path
+            fill="#242A45"
+            fill-rule="evenodd"
+            d="M0 0h18v3H0V0zm0 6h18v3H0V6zm0 6h18v3H0v-3z"
+          />
+        </svg>
+      </span>
+    </header>
+    <aside class="container-content">
+      <main class="container-content__info">
+        <span class="info"
+          ><svg
+            height="320px"
+            width="320px"
+            version="1.1"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            viewBox="-153.6 -153.6 819.20 819.20"
+            xml:space="preserve"
+            fill="#7a4d4d"
+            stroke="#7a4d4d"
+            stroke-width="0.0051200099999999995"
+            transform="rotate(0)"
+          >
+            <g
+              id="SVGRepo_bgCarrier"
+              stroke-width="0"
+              transform="translate(0,0), scale(1)"
+            >
+              <path
+                transform="translate(-153.6, -153.6), scale(25.6)"
+                d="M16,29.645063654830057C18.556912972850125,29.96109922115148,21.116410587640228,28.701783435007478,23.130237162405745,27.094865591221566C25.04021838113189,25.57081037210501,25.59564744621067,23.04815422151581,26.875791234238136,20.96680743427018C28.377893573893857,18.524584999574827,31.604466995947334,16.65086452525127,31.307897605249213,13.799056597667533C31.013256998402944,10.965795833924107,27.67106685597015,9.61320101500299,25.50295623850076,7.7656415455982195C23.658074049008654,6.19352177842934,21.825235652025004,4.720347955733054,19.580594833399918,3.805613868257119C17.130497772464878,2.807152430388997,14.464212256951505,1.3882153970216347,11.961869195677984,2.247389450754909C9.468229442406088,3.1035752425938132,8.627756805815993,6.1238370920984195,6.936916427564931,8.146793380435007C5.283749204550382,10.124676729144962,2.3258512312945965,11.435695751286346,2.133466199142216,14.006293426489034C1.9400826265815616,16.59023333327484,4.551148186117688,18.420285136342077,6.00765395977626,20.56335153272736C7.2064853085687295,22.32728218442462,8.340873049810526,24.027517449797177,9.919160643561574,25.461970731802634C11.777427695249374,27.150888063276398,13.507869055613062,29.337035175675936,16,29.645063654830057"
+                fill="#38a9f0"
+                strokewidth="0"
+              />
+            </g>
+
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke="#CCCCCC"
+              stroke-width="8.192015999999999"
+            />
+
+            <g id="SVGRepo_iconCarrier">
+              <path
+                style="fill: #cad1d8"
+                d="M24,16h464c13.254,0,24,10.745,24,24v432c0,13.254-10.745,24-24,24H24C10.745,496,0,485.255,0,472V40 C0,26.745,10.745,16,24,16z"
+              />
+              <g>
+                <circle
+                  style="fill: #9ba7af"
+                  cx="24.002"
+                  cy="47.995"
+                  r="7.998"
+                />
+                <circle
+                  style="fill: #9ba7af"
+                  cx="56.002"
+                  cy="47.995"
+                  r="7.998"
+                />
+                <circle
+                  style="fill: #9ba7af"
+                  cx="88.002"
+                  cy="47.995"
+                  r="7.998"
+                />
+              </g>
+              <path
+                style="fill: #ffffff"
+                d="M24,80h464c4.418,0,8,3.582,8,8v384c0,4.418-3.582,8-8,8H24c-4.418,0-8-3.582-8-8V88 C16,83.582,19.582,80,24,80z"
+              />
+              <path
+                style="fill: #e2e5e7"
+                d="M120,40h368c4.418,0,8,3.582,8,8l0,0c0,4.418-3.582,8-8,8H120c-4.418,0-8-3.582-8-8l0,0 C112,43.582,115.582,40,120,40z"
+              />
+              <g>
+                <path
+                  style="fill: #cad1d8"
+                  d="M144,232.001h176c4.418,0,8,3.582,8,8l0,0c0,4.418-3.582,8.001-8,8.001H144 c-4.418,0-8.001-3.582-8.001-8.001l0,0C136.001,235.582,139.582,232.001,144,232.001z"
+                />
+                <path
+                  style="fill: #cad1d8"
+                  d="M144,264h136c4.418,0,8,3.582,8,8l0,0c0,4.418-3.582,8-8,8H144c-4.418,0-8.001-3.582-8.001-8l0,0 C136.001,267.582,139.582,264,144,264z"
+                />
+                <path
+                  style="fill: #cad1d8"
+                  d="M144,312h176c4.418,0,8,3.582,8,8l0,0c0,4.418-3.582,8-8,8H144c-4.418,0-8.001-3.582-8.001-8l0,0 C136.001,315.582,139.582,312,144,312z"
+                />
+                <path
+                  style="fill: #cad1d8"
+                  d="M144,344h136c4.418,0,8,3.582,8,8l0,0c0,4.418-3.582,8-8,8H144c-4.418,0-8.001-3.582-8.001-8l0,0 C136.001,347.582,139.582,344,144,344z"
+                />
+              </g>
+              <path
+                style="fill: #ee2c39"
+                d="M392,232.001h32c13.254,0,24,10.745,24,24.001l0,0c0,13.254-10.745,24-24,24h-32 c-13.254,0-24-10.745-24-24l0,0C368,242.746,378.745,232.001,392,232.001z"
+              />
+              <path
+                style="fill: #785cad"
+                d="M24,80h464c4.418,0,8,3.582,8,8v64l0,0H16l0,0V88C16,83.582,19.582,80,24,80z"
+              />
+              <circle style="fill: #abe1fa" cx="256" cy="152.002" r="39.998" />
+              <path
+                style="fill: #a2a9ae"
+                d="M289.2,174.24c-1.252-2.926-3.449-5.348-6.24-6.88l-11.44-6.88c-1.305-0.764-2.703-1.356-4.16-1.76 v-5.12c7.034-6.252,7.668-17.022,1.417-24.056c-6.252-7.034-17.022-7.668-24.056-1.417c-7.034,6.252-7.668,17.022-1.417,24.056 c0.444,0.499,0.917,0.972,1.417,1.417v5.12c-1.457,0.404-2.856,0.995-4.16,1.76l-11.44,6.88c-2.79,1.531-4.987,3.954-6.24,6.88 c12.354,18.314,37.216,23.144,55.53,10.79C282.666,182.159,286.329,178.495,289.2,174.24z"
+              />
+              <circle style="fill: #ffffff" cx="423.998" cy="256" r="15.995" />
+              <g>
+                <circle style="fill: #cad1d8" cx="96" cy="248.002" r="15.995" />
+                <circle style="fill: #cad1d8" cx="96" cy="327.998" r="15.995" />
+                <circle style="fill: #cad1d8" cx="96" cy="408.002" r="15.995" />
+              </g>
+              <path
+                style="fill: #3cb54a"
+                d="M392,312h32c13.254,0,24,10.745,24,24l0,0c0,13.254-10.745,24-24,24h-32c-13.254,0-24-10.745-24-24 l0,0C368,322.746,378.745,312,392,312z"
+              />
+              <circle
+                style="fill: #ffffff"
+                cx="391.998"
+                cy="335.995"
+                r="15.995"
+              />
+              <g>
+                <path
+                  style="fill: #cad1d8"
+                  d="M144,392h176c4.418,0,8,3.582,8,8l0,0c0,4.418-3.582,8-8,8H144c-4.418,0-8.001-3.582-8.001-8l0,0 C136.001,395.582,139.582,392,144,392z"
+                />
+                <path
+                  style="fill: #cad1d8"
+                  d="M144,424h136c4.418,0,8,3.582,8,8l0,0c0,4.418-3.582,8-8,8H144c-4.418,0-8.001-3.582-8.001-8l0,0 C136.001,427.582,139.582,424,144,424z"
+                />
+              </g>
+              <path
+                style="fill: #3cb54a"
+                d="M392,392h32c13.254,0,24,10.745,24,24l0,0c0,13.254-10.745,24-24,24h-32c-13.254,0-24-10.745-24-24 l0,0C368,402.746,378.745,392,392,392z"
+              />
+              <circle style="fill: #ffffff" cx="391.998" cy="416" r="15.995" />
+            </g>
+          </svg>
+        </span>
+        <div class="bookmark-manager">
+          <div class="bookmark-manager__text">
+            <h1 class="bookmark-manager__text_title">
+              A Simple Bookmark Manager
+            </h1>
+            <p class="bookmark-manager__text_paragraph">
+              A clean and simple interface to organize your favorite websites.
+              Open a new browser tab and see your sites load instantly. Try it
+              for free.
+            </p>
+          </div>
+          <div class="bookmark-manager__buttons">
+            <button class="bookmark-manager__buttons_chrome">
+              Get it on Chrome
+            </button>
+            <button class="bookmark-manager__buttons_firefox">
+              Get it on Firefox
+            </button>
+          </div>
+        </div>
+        <div class="bookmark-feature__text">
+          <h1 class="bookmark-feature__text_title">Features</h1>
+          <p class="bookmark-feature__text_paragraph">
+            Our aim is to make it easy and quick for you to access your
+            favourite websites. Your bookmarks sync between you devices so you
+            can access them on the go.
+          </p>
+        </div>
+      </main>
+    </aside>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+</script>
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Playwrite+AR:wght@100..400&family=Playwrite+BE+VLG:wght@100..400&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap");
+
+* {
+  margin: 0;
+}
+.container {
+  box-sizing: border-box;
+  min-width: 375px;
+  display: grid;
+  grid-template-columns: auto;
+  background-color: #fff;
+  min-height: 100vh;
+
+  &-header {
+    padding: 1rem;
+    display: grid;
+    max-width: 375px;
+    grid-template-columns: auto auto;
+    justify-content: space-between;
+    height: 1rem;
+  }
+  &-menu {
+    cursor: pointer;
+  }
+  &-content {
+    display: flex;
+    justify-content: end;
+    flex-direction: column;
+    padding: 1rem;
+    &__info {
+      padding: 1rem;
+
+      .info {
+        display: flex;
+        justify-content: center;
+      }
+      .bookmark-manager {
+        display: flex;
+        flex-direction: column;
+        justify-content: start;
+        width: 320px;
+
+        &__text {
+          &_title {
+            font-family: Rubik;
+            font-size: 18px;
+            display: flex;
+            text-align: center;
+            justify-content: center;
+            margin: 1rem 0;
+            font-weight: 500;
+          }
+          &_paragraph {
+            font-family: Roboto;
+            font-size: 14px;
+            display: flex;
+            text-align: center;
+            justify-content: center;
+            margin: 1rem 1rem 1rem 2rem;
+            font-weight: 400;
+            color: #a9a4a4;
+          }
+        }
+        &__buttons {
+          display: grid;
+          grid-template-columns: 140px 140px;
+          height: 45px;
+          column-gap: 1rem;
+          margin-left: 1rem;
+          margin-top: 2rem;
+          &_chrome {
+            background-color: hsl(231, 69%, 60%);
+            cursor: pointer;
+            border: none;
+            color: #fff;
+            border-radius: 8px;
+            font-size: 14px;
+            font-family: Rubik;
+          }
+          &_firefox {
+            background-color: #eaeaea;
+            border: none;
+            color: #000;
+            font-size: 14px;
+            border-radius: 8px;
+            box-shadow: 3px 2px 5px 0 grey;
+            font-family: Rubik;
+          }
+        }
+      }
+    }
+    .bookmark-feature__text {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      width: 360px;
+      
+      &_title {
+        display: flex;
+        justify-content: center;
+        margin-top: 10rem;
+        font-family: Rubik;
+        font-size: 18px;
+        font-weight: 500
+      }
+      
+      &_paragraph {
+        font-family: "Roboto";
+        text-align: center;
+        font-weight: 400;
+        color: #a9a4a4;
+        padding: 2rem;
+        font-size: 14px;
+        justify-content: center;
+      }
+    }
+  }
+}
+</style>
